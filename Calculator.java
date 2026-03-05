dfde82d302442cbfe6fe6e7ea977d026d582f469
Abhijit Tarawade <abhitar@yahoo.com>
2026-03-05 18:52:14 +0500

    Version 3: Added multiplication and division operations

--- Calculator.java
+++ Calculator.java
@@ -8,12 +8,22 @@ public class Calculator {
         return a - b;
     }
 
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        return (double)a / b;
+    }
+
     public static void main(String[] args) {
 
-        int a = 10;
-        int b = 5;
+        int a = 20;
+        int b = 4;
 
         System.out.println("Addition: " + add(a,b));
         System.out.println("Subtraction: " + subtract(a,b));
+        System.out.println("Multiplication: " + multiply(a,b));
+        System.out.println("Division: " + divide(a,b));
     }
 }
